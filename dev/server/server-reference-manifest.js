64c5d5e239b4b39d96fbe573ab8f95de9b1e6a9f
travelstoryadmin <travelstoryadmin@gmail.com>
2026-09-15 08:55:42 +0000

    Remove build cache from repository

--- dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MQn6dGXu34NTpUvnDAzS74dtgpWVVcnvEJ4O9E+LCDM=\"\n}"
--- dev/static/chunks/_02la4bt._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_globals_0yg4wg8.css","static/chunks/_0fyzip_._.js","static/chunks/_0n0idp4._.js","static/chunks/_1u7mxvt._.js"],
-    source: "entry"
-});
